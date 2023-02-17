df925473144897ded1f4fdc3c0309b6cddff215f
JPDovale <joaopaulodovaledeo.60@gmail.com>
2023-02-17 17:36:39 -0300

    correction errors on refactory

--- src/pages/project/[id]/plot/historicalFact/index.page.tsx
+++ src/pages/project/[id]/plot/historicalFact/index.page.tsx
@@ -1,57 +1,45 @@
+import { IUpdatePlotDTO } from '@api/dtos/IUpdatePlotDTO'
+import { EditorAndComments } from '@components/ProjectsComponents/EditorAndComments'
+import { ProjectsContext } from '@contexts/projects'
+import { usePreventBack } from '@hooks/usePreventDefaultBack'
+import { useProject } from '@hooks/useProject'
+import { ProjectPageLayout } from '@layouts/ProjectPageLayout'
 import { NextSeo } from 'next-seo'
 import { useRouter } from 'next/router'
 import { useContext, useState } from 'react'
-import { IUpdatePlotDTO } from '../../../../../api/dtos/IUpdatePlotDTO'
-import { IProjectResponse } from '../../../../../api/responsesTypes/IProjcetResponse'
-import { EditorAndComments } from '../../../../../components/EditorAndComments'
-import { ProjectsContext } from '../../../../../contexts/projects'
-import { UserContext } from '../../../../../contexts/user'
-import { usePreventBack } from '../../../../../hooks/usePreventDefaultBack'
-import { ProjectPageLayout } from '../../../../../layouts/ProjectPageLayout'
 
 export default function HistoricalFactPage() {
   const [historicalFact, setHistoricalFact] = useState('')
   const [message, setMessage] = useState('')
 
-  const { projects, loading, updatePlot } = useContext(ProjectsContext)
-  const { user } = useContext(UserContext)
+  const { loading, updatePlot } = useContext(ProjectsContext)
 
   const router = useRouter()
   const { id } = router.query
   usePreventBack(`/project/${id}/plot`)
 
-  const project = projects.find(
-    (project) => project.id === id,
-  ) as IProjectResponse
-
+  const { project, projectName, permission } = useProject(id as string)
   const commentsHistoricalFact = project?.plot.comments?.filter(
     (comment) => comment.to === 'historicalFact',
   )
 
-  const userInProject = project?.users.find((u) => u.id === user?.id)
-
   async function handleUpdateHistoricalFact() {
     setMessage('')
-
     if (historicalFact === project?.plot.historicalFact) return
-
     const updatedPlotHistoricalFact: IUpdatePlotDTO = {
       historicalFact,
     }
 
     await updatePlot(updatedPlotHistoricalFact, project.id as string)
-
     setMessage('Fato histórico atualizado com sucesso.')
   }
 
   return (
     <>
-      <NextSeo
-        title={`${project?.name || 'Carregando...'}-Fato histórico | Ognare`}
-        noindex
-      />
+      <NextSeo title={`${projectName}-Fato histórico | Ognare`} noindex />
+
       <ProjectPageLayout
-        projectName={project?.name}
+        projectName={projectName}
         projectId={`${id}`}
         paths={['Plot', 'Fato histórico']}
         loading={loading}
@@ -63,7 +51,7 @@ export default function HistoricalFactPage() {
           updateValue={handleUpdateHistoricalFact}
           value={historicalFact}
           preValue={project?.plot.historicalFact}
-          permission={userInProject?.permission}
+          permission={permission}
           comments={commentsHistoricalFact}
           projectCreatedPerUser={project?.createdPerUser}
           projectId={project?.id as string}
