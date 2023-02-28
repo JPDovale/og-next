9388d20beff8215f6220e97fbd3c0d6bc5d5a340
JPDovale <joaopaulodovaledeo.60@gmail.com>
2023-02-28 18:53:00 -0300

    update api url

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3030',
-  // baseURL: 'https://ogapi.onrender.com',
+  // baseURL: 'http://localhost:3030',
+  baseURL: 'https://ogapi.onrender.com',
 })
 
 export async function GetInfoUser() {
